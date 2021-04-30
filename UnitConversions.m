803eb56c1adf1f933b2b864519a2352860fe87e2
PhoenixWings18 <masterkaterihawk@gmail.com>
2021-04-30 17:56:24 -0500

    Fixed unit lables throughout function

--- UnitConversions.m
+++ UnitConversions.m
@@ -7,16 +7,16 @@ function [] = radioSelect(~,~)
     inputVar = gui.buttonGroup1.SelectedObject.String;
     convertTo = gui.buttonGroup2.SelectedObject.String;
 
-    if strcmp(inputVar, 'Pounds (lbs)') && ~(strcmp(convertTo, 'Newtons (N)') || ...
-            strcmp(convertTo, 'Kilograms (kg)'))
+    if strcmp(inputVar, 'Pounds') && ~(strcmp(convertTo, 'Newtons') || ...
+            strcmp(convertTo, 'Kilograms'))
         errordlg('Input units cannot be converted to output units. Please select different output units.', 'Error', 'modal');
         return;
-    elseif strcmp(inputVar, 'Fahrenheit (F)') && ~(strcmp(convertTo, 'Kelvin (K)') || ...
-            strcmp(convertTo, 'Celsius (C)'))
+    elseif strcmp(inputVar, 'Fahrenheit') && ~(strcmp(convertTo, 'Kelvin') || ...
+            strcmp(convertTo, 'Celsius'))
         errordlg('Input units cannot be converted to output units. Please select different output units.', 'Error', 'modal');
         return;
-    elseif strcmp(inputVar, 'Joules (J)') && ~(strcmp(convertTo, 'Tons of TNT') || ...
-            strcmp(convertTo, 'British Thermal Units (BTU)'))
+    elseif strcmp(inputVar, 'Joules') && ~(strcmp(convertTo, 'Tons of TNT') || ...
+            strcmp(convertTo, 'British Thermal Units'))
         errordlg('Input units cannot be converted to output units. Please select different output units.', 'Error', 'modal');
         return;
     end
@@ -38,17 +38,17 @@ function [] = conversion(number, inputVar, convertTo)
     %Do I need to restate the input variables to this function, or will it
     %just work? Need to test this with the completed GUI_Control.
     
-    if strcmp(inputVar, 'Joules (J)') & strcmp(convertTo, 'British Thermal Units (BTU)')
+    if strcmp(inputVar, 'Joules') && strcmp(convertTo, 'British Thermal Units')
         compute = number/1055.05585;
-    elseif strcmp(inputVar, 'Joules (J)') & strcmp(convertTo, 'Tons of TNT')
+    elseif strcmp(inputVar, 'Joules') && strcmp(convertTo, 'Tons of TNT')
         compute = number/4184000000;
-    elseif strcmp(inputVar, 'Pounds (lbs)') & strcmp(convertTo, 'Newtons (N)')
+    elseif strcmp(inputVar, 'Pounds') && strcmp(convertTo, 'Newtons')
         compute = number/0.22480894244319;
-    elseif strcmp(inputVar, 'Pounds (lbs)') & strcmp(convertTo, 'Kilograms (kg)')
+    elseif strcmp(inputVar, 'Pounds') && strcmp(convertTo, 'Kilograms')
         compute = (number/0.22480894244319)/9.81;
-    elseif strcmp(inputVar, 'Fahrenheit (F)') & strcmp(convertTo, 'Celsius (C)')
+    elseif strcmp(inputVar, 'Fahrenheit') && strcmp(convertTo, 'Celsius')
         compute = (number - 32)*(5/9);
-    elseif strcmp(inputVar, 'Fahrenheit (F)') & strcmp(convertTo, 'Kelvin (K)')
+    elseif strcmp(inputVar, 'Fahrenheit') && strcmp(convertTo, 'Kelvin')
         compute = (number - 32)*(5/9) + 273.15;
     else 
         errordlg('Please select the input units and the desired conversion units', 'Error', 'modal');
@@ -61,12 +61,7 @@ end
 %{
 Need to make sure the dispConversion can access the compute variable and
 be called by the gui function, then display the result in the non-editable text box
-when the pushbutton "convert" is pressed. Will need to see the pushbutton
-function in the GUI before I can write this. 
-
-Note: DO NOT create a non-editable text box in the GUI! It will not appear until the button is
-pushed, the whole point of dispConversion is to display it after the
-button is pushed.
+when the pushbutton "convert" is pressed. 
 %}
 
 function [] = dispConversion(~, ~, compute)
