954b222bde101a01e8b8ac8d67ad0a1386a3e193
PhoenixWings18 <masterkaterihawk@gmail.com>
2021-04-30 19:32:45 -0500

    Created mostly functional unit converter. Logic works, but displaying the answer is not working yet.

--- unitConverter.m
+++ unitConverter.m
@@ -19,7 +19,7 @@ function [] = unitConverter()
     gui.endUnit(5) = uicontrol(gui.buttonGroupEnd,'style','radiobutton','unit','normalized','position',[.1 .3 1 .1],'string','Newtons');
     gui.endUnit(6) = uicontrol(gui.buttonGroupEnd,'style','radiobutton','unit','normalized','position',[.1 .2 1 .1],'string','Kilograms');
     
-    gui.convert = uicontrol('style','pushbutton','unit','normalized','position',[.37 .1 .25 .1],'string','Convert Units','callback', {@event});
+    gui.convert = uicontrol('style','pushbutton','unit','normalized','position',[.37 .1 .25 .1],'string','Convert Units','callback', {@dispConversion});
    
     
 end
@@ -28,11 +28,52 @@ function [] = radioSelect(~,~)
 
     global gui;
     
-%Need to find out if return or break is the correct command to use.
+    input = gui.buttonGroupStart.SelectedObject.String;
+    convertTo = gui.buttonGroupEnd.SelectedObject.String;
+    num = str2num(gui.startNumber.String);
+    
+    compute = 0;
+    
+    if strcmp(input, 'Joules') && strcmp(convertTo, 'British Thermal Units')
+        compute = num/1055.05585;
+        
+    elseif strcmp(input, 'Joules') && strcmp(convertTo, 'Tons of TNT')
+        compute = num/4184000000;
+        
+    elseif strcmp(input, 'Pounds') && strcmp(convertTo, 'Newtons')
+        compute = num/0.22480894244319;
+        
+    elseif strcmp(input, 'Pounds') && strcmp(convertTo, 'Kilograms')
+        compute = (num/0.22480894244319)/9.81;
+        
+    elseif strcmp(input, 'Fahrenheit') && strcmp(convertTo, 'Celsius')
+        compute = (num - 32)*(5/9);
+        
+    elseif strcmp(input, 'Fahrenheit') && strcmp(convertTo, 'Kelvin')
+        compute = (num - 32)*(5/9) + 273.15;
+   
+    end
+    
+    gui.Conversion = num2str(compute);
+    
+end
 
-    inputVar = gui.buttonGroup1.SelectedObject.String;
-    convertTo = gui.buttonGroup2.SelectedObject.String;
+function [] = dispConversion(~, ~)
 
+    global gui;
+    
+    
+     if isnan(str2double(gui.startNumber.String)) || (strlength(gui.startNumber.String) == 0)
+        errordlg('Only real, rational numeric values are allowed. Please enter a number in the input box.', 'Error', 'modal');
+        return;
+        
+     else
+         
+     end   
+     
+    inputVar = gui.buttonGroupStart.SelectedObject.String;
+    convertTo = gui.buttonGroupEnd.SelectedObject.String;
+    
     if strcmp(inputVar, 'Pounds') && ~(strcmp(convertTo, 'Newtons') || ...
             strcmp(convertTo, 'Kilograms'))
         errordlg('Input units cannot be converted to output units. Please select different output units.', 'Error', 'modal');
@@ -47,70 +88,19 @@ function [] = radioSelect(~,~)
             strcmp(convertTo, 'British Thermal Units'))
         errordlg('Input units cannot be converted to output units. Please select different output units.', 'Error', 'modal');
         return;
-    end
-
-    if isnan(str2double(gui.Text.String)) || gui.Text.String == ''
-        errordlg('Only real, rational numeric values are allowed. Please enter a number in the input box.', 'Error', 'modal');
-        return;
-        
-    else
-        number = str2double(gui.Text.String);
-        conversion(number, inputVar, convertTo);
-    end
-
-end
-
-function [] = conversion(number, inputVar, convertTo)
-
-    global gui; 
     
-    %Do I need to restate the input variables to this function, or will it
-    %just work? Need to test this with the completed GUI_Control.
+    else   
     
-    if strcmp(inputVar, 'Joules') && strcmp(convertTo, 'British Thermal Units')
-        compute = number/1055.05585;
-        
-    elseif strcmp(inputVar, 'Joules') && strcmp(convertTo, 'Tons of TNT')
-        compute = number/4184000000;
-        
-    elseif strcmp(inputVar, 'Pounds') && strcmp(convertTo, 'Newtons')
-        compute = number/0.22480894244319;
-        
-    elseif strcmp(inputVar, 'Pounds') && strcmp(convertTo, 'Kilograms')
-        compute = (number/0.22480894244319)/9.81;
-        
-    elseif strcmp(inputVar, 'Fahrenheit') && strcmp(convertTo, 'Celsius')
-        compute = (number - 32)*(5/9);
-        
-    elseif strcmp(inputVar, 'Fahrenheit') && strcmp(convertTo, 'Kelvin')
-        compute = (number - 32)*(5/9) + 273.15;
-        
-    else 
-        errordlg('Please select the input units and the desired conversion units', 'Error', 'modal');
-   
-    end
+    answer = str2num(gui.Conversion);
     
-     dispConversion(compute);
-     
-end
-
-%{
-Need to make sure the dispConversion can access the compute variable and
-be called by the gui function, then display the result in the non-editable text box
-when the pushbutton "convert" is pressed. 
-%}
-
-function [] = dispConversion(~, ~, compute)
-
-    global gui;
-    
-    answerString = [gui.Text.String, ' ', gui.buttonGroup1.SelectedObject.String, ' equals ', ...
-       compute, ' ', gui.buttonGroup2.SelectedObject.String, '.'];
+    answerString = [gui.startNumber.String, ' ', gui.buttonGroupStart.SelectedObject.String, ' equals ', ...
+       answer, ' ', gui.buttonGroupEnd.SelectedObject.String, '.'];
    
     gui.answer = uicontrol('style', 'text', 'units', 'normalized', 'position',...
-    [.15 .78 .5 .05], 'string', answerString, 'horizontalalignment', 'right');
-    
+    [.19 .23 .5 .1], 'string', answerString, 'horizontalalignment', 'right');
 
+    end
+    
 end
 
 %{
